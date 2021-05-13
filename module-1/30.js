45b29f3c97e606e7c0cda6689af633cfc691e151
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-05-13 22:07:08 +0300

    add module-1

---
+++ module-1/30.js
@@ -0,0 +1,30 @@
+function getNameLength(name) {
+  const message = `Name ${name} is ${name.length} characters long`; // Change this line
+console.log(message)
+  return message;
+}
+
+getNameLength("Poly");
+getNameLength("Harambe");
+getNameLength("Billy");
+getNameLength("Joe");
+
+// Длина строки
+// Для того чтобы узнать длину строки, то есть количество её символов, у строк есть встроенное свойство length, значение которого можно получить обратившить к нему через точку после имени переменной или строкового литерала.
+
+// const productName = "Repair droid";
+
+// // Если в переменной хранится строка
+// console.log(productName.length); // 12
+
+// // Если строковый литерал
+// console.log("Repair droid".length); // 12
+// Задание
+// Функция getNameLength(name) принимает имя (параметр name) и возвращает строку, в которой указана его длина. Дополни шаблонную строку в переменной message длиной строки из параметра name.
+
+// Тесты
+// Объявлена функция getNameLength(name)
+// Вызов функции getNameLength("Poly") возвращает "Name Poly is 4 characters long"
+// Вызов функции getNameLength("Harambe") возвращает "Name Harambe is 6 characters long"
+// Вызов функции getNameLength("Billy") возвращает "Name Billy is 5 characters long"
+// Вызов функции getNameLength("Joe") возвращает "Name Joe is 3 characters long"
---
+++ module-1/32.js
@@ -0,0 +1,28 @@
+function getSubstring(string, length) {
+  const substring = string.slice(0, length); // Change this line
+
+  return substring;
+}
+
+// Метод slice()
+// Метод строк slice(startIndex, endIndex) используется для создания копии части или всей строки. Он делает копию элементов строки от startIndex и до, но не включая endIndex и возвращает новую строку.
+
+// const productName = "Repair droid";
+// console.log(productName.slice(0, 4)); // "Repa"
+// console.log(productName.slice(3, 9)); // "air dr"
+// console.log(productName.slice(0, productName.length)); // "Repair droid"
+// console.log(productName.slice(7, productName.length)); // "droid"
+// Задание
+// Функция getSubstring(string, length) принимает строку и возвращает подстроку от начала и до length символов. Она объявляет два параметра, значения которых будут задаваться во время её вызова:
+
+// string - оригинальная строка
+// length - количество символов с начала строки для подстроки
+// Присвой переменной substring выражение создания подстроки длинной length символов (от начала) из строки string.
+
+// Тесты
+// Объявлена функция getSubstring(string, length)
+// Вызов функции getSubstring("Hello world", 3) возвращает "Hel"
+// Вызов функции getSubstring("Hello world", 6) возвращает "Hello"
+// Вызов функции getSubstring("Hello world", 8) возвращает "Hello wo"
+// Вызов функции getSubstring("Hello world", 11) возвращает "Hello world"
+// Вызов функции getSubstring("Hello world", 0) возвращает ""
