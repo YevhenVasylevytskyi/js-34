7486f89aa5104b23932b415b701eb914a9c99ffa
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-05-17 22:50:00 +0300

    q8

---
+++ module-2/7.js
@@ -0,0 +1,16 @@
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const fruitsArrayLength = fruits.length;
+
+// Длина массива
+// Длина массива, то есть число его элементов, хранится в свойстве length. Это динамическая величина, которая изменяется автоматически при добавлении или удалении элементов.
+
+// const planets = ["Earth", "Mars", "Venus"];
+// console.log(planets.length); // 3
+// Задание
+// Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+
+// Тесты
+// Объявлена переменная fruitsArrayLength
+// Значение переменной fruitsArrayLength это число 4
---
+++ module-2/9.js
@@ -0,0 +1,21 @@
+function getExtremeElements(array) {
+  // Change code below this line
+//   const array = [];
+// console.log (getExtremeElements[0])/*, getExtremeElements.length -1*/;
+
+  // Change code above this line
+}
+
+getExtremeElements([1, 2, 3, 4, 5]);
+getExtremeElements(["Earth", "Mars", "Venus"]);
+getExtremeElements(["apple", "peach", "pear", "banana"]);
+console.log (getExtremeElements[0]);
+// Задача: Крайние элементы массива
+// Задание
+// Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array.
+
+// Тесты
+// Объявлена функция getExtremeElements(array)
+// Вызов getExtremeElements([1, 2, 3, 4, 5]) возвращает [1, 5]
+// Вызов getExtremeElements(["Earth", "Mars", "Venus"]) возвращает ["Earth", "Venus"]
+// Вызов getExtremeElements(["apple", "peach", "pear", "banana"]) возвращает ["apple", "banana"]
