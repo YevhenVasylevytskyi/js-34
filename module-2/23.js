2f54ab8627172c397c517e7967a7ce02016ff685
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-05-19 22:22:10 +0300

    q25

--- module-2/23.js
+++ module-2/23.js
@@ -1,3 +1,18 @@
+function filterArray(numbers, value) {
+   // Change code below this line
+  let result = [];
+  
+    for(let i = 0; i < numbers.length; i += 1){
+  		if (numbers[i] > value) {
+          result.push(numbers[i]);
+        }        
+    }
+  return result;
+  // Change code above this line
+}
+
+filterArray([1, 2, 3, 4, 5], 3);
+filterArray([1, 2, 3, 4, 5], 5);
 
 
 // Задача: фильтрация массива чисел
